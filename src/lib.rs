b74bd08b129973f0aa88dc56bc10dfc4328882cd
WAYAN MURDIONI <154336943+jaka2m@users.noreply.github.com>
2025-04-14 08:05:09 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -24,23 +24,13 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
     let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
     let main_page_url = env.var("MAIN_PAGE_URL").map(|x|x.to_string()).unwrap();
     let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let link_page_url = env.var("LINK_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let config = Config { 
-        uuid, 
-        host: host.clone(), 
-        proxy_addr: host, 
-        proxy_port: 443, 
-        main_page_url, 
-        sub_page_url,
-        link_page_url
-    };
+    let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url, sub_page_url};
 
     Router::with_data(config)
         .on_async("/", fe)
         .on_async("/sub", sub)
-        .on_async("/link", link)  // Changed to on_async
+        .on("/link", link)
         .on_async("/:proxyip", tunnel)
-        .on_async("/Geo-Project/:proxyip", tunnel)
         .run(req, env)
         .await
 }
@@ -59,10 +49,6 @@ async fn sub(_: Request, cx: RouteContext<Config>) -> Result<Response> {
     get_response_from_url(cx.data.sub_page_url).await
 }
 
-// Changed to fetch from URL like fe and sub
-async fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
-    get_response_from_url(cx.data.link_page_url).await
-}
 
 async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> {
     let mut proxyip = cx.param("proxyip").unwrap().to_string();
@@ -121,4 +107,35 @@ async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> 
     } else {
         Response::from_html("hi from wasm!")
     }
+
+}
+
+fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    let host = cx.data.host.to_string();
+    let uuid = cx.data.uuid.to_string();
+
+    let vmess_link = {
+        let config = json!({
+            "ps": "siren vmess",
+            "v": "2",
+            "add": host,
+            "port": "80",
+            "id": uuid,
+            "aid": "0",
+            "scy": "zero",
+            "net": "ws",
+            "type": "none",
+            "host": host,
+            "path": "/KR",
+            "tls": "",
+            "sni": "",
+            "alpn": ""}
+        );
+        format!("vmess://{}", URL_SAFE.encode(config.to_string()))
+    };
+    let vless_link = format!("vless://{uuid}@{host}:443?encryption=none&type=ws&host={host}&path=%2FKR&security=tls&sni={host}#siren vless");
+    let trojan_link = format!("trojan://{uuid}@{host}:443?encryption=none&type=ws&host={host}&path=%2FKR&security=tls&sni={host}#siren trojan");
+    let ss_link = format!("ss://{}@{host}:443?plugin=v2ray-plugin%3Btls%3Bmux%3D0%3Bmode%3Dwebsocket%3Bpath%3D%2FKR%3Bhost%3D{host}#siren ss", URL_SAFE.encode(format!("none:{uuid}")));
+    
+    Response::from_body(ResponseBody::Body(format!("{vmess_link}\n{vless_link}\n{trojan_link}\n{ss_link}").into()))
 }
