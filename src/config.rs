03557ea536dd918c448840bb1982ed8f784f9119
WAYAN MURDIONI <154336943+jaka2m@users.noreply.github.com>
2025-04-14 00:21:09 -0700

    Add files via upload

---
+++ src/config.rs
@@ -0,0 +1,13 @@
+use uuid::Uuid;
+
+pub struct Config {
+    pub uuid: Uuid,
+    pub host: String,
+    pub proxy_addr: String,
+    pub proxy_port: u16,
+
+    pub main_page_url: String,
+    pub sub_page_url: String,
+    pub link_page_url: String,
+    
+}
---
+++ src/lib.rs
@@ -0,0 +1,124 @@
+mod common;
+mod config;
+mod proxy;
+
+use crate::config::Config;
+use crate::proxy::*;
+
+use std::collections::HashMap;
+use base64::{engine::general_purpose::URL_SAFE, Engine as _};
+use serde_json::json;
+use uuid::Uuid;
+use worker::*;
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+static PROXYIP_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^.+-\d+$").unwrap());
+static PROXYKV_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([A-Z]{2})").unwrap());
+
+#[event(fetch)]
+async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
+    let uuid = env
+        .var("UUID")
+        .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
+    let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
+    let main_page_url = env.var("MAIN_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let link_page_url = env.var("LINK_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let config = Config { 
+        uuid, 
+        host: host.clone(), 
+        proxy_addr: host, 
+        proxy_port: 443, 
+        main_page_url, 
+        sub_page_url,
+        link_page_url
+    };
+
+    Router::with_data(config)
+        .on_async("/", fe)
+        .on_async("/sub", sub)
+        .on_async("/link", link)  // Changed to on_async
+        .on_async("/:proxyip", tunnel)
+        .on_async("/Geo-Project/:proxyip", tunnel)
+        .run(req, env)
+        .await
+}
+
+async fn get_response_from_url(url: String) -> Result<Response> {
+    let req = Fetch::Url(Url::parse(url.as_str())?);
+    let mut res = req.send().await?;
+    Response::from_html(res.text().await?)
+}
+
+async fn fe(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    get_response_from_url(cx.data.main_page_url).await
+}
+
+async fn sub(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    get_response_from_url(cx.data.sub_page_url).await
+}
+
+// Changed to fetch from URL like fe and sub
+async fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    get_response_from_url(cx.data.link_page_url).await
+}
+
+async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> {
+    let mut proxyip = cx.param("proxyip").unwrap().to_string();
+    if PROXYKV_PATTERN.is_match(&proxyip)  {
+        let kvid_list: Vec<String> = proxyip.split(",").map(|s|s.to_string()).collect();
+        let kv = cx.kv("SIREN")?;
+        let mut proxy_kv_str = kv.get("proxy_kv").text().await?.unwrap_or("".to_string());
+        let mut rand_buf = [0u8, 1];
+        getrandom::getrandom(&mut rand_buf).expect("failed generating random number");
+        
+        if proxy_kv_str.len() == 0 {
+            console_log!("getting proxy kv from github...");
+            let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json")?);
+            let mut res = req.send().await?;
+            if res.status_code() == 200 {
+                proxy_kv_str = res.text().await?.to_string();
+                kv.put("proxy_kv", &proxy_kv_str)?.expiration_ttl(60 * 60 * 24).execute().await?; // 24 hours
+            } else {
+                return Err(Error::from(format!("error getting proxy kv: {}", res.status_code())));
+            }
+        }
+        
+        let proxy_kv: HashMap<String, Vec<String>> = serde_json::from_str(&proxy_kv_str)?;
+        
+        // select random KV ID
+        let kv_index = (rand_buf[0] as usize) % kvid_list.len();
+        proxyip = kvid_list[kv_index].clone();
+        
+        // select random proxy ip
+        let proxyip_index = (rand_buf[0] as usize) % proxy_kv[&proxyip].len();
+        proxyip = proxy_kv[&proxyip][proxyip_index].clone().replace(":", "-");
+    }
+
+    if PROXYIP_PATTERN.is_match(&proxyip) {
+        if let Some((addr, port_str)) = proxyip.split_once('-') {
+            if let Ok(port) = port_str.parse() {
+                cx.data.proxy_addr = addr.to_string();
+                cx.data.proxy_port = port;
+            }
+        }
+    }
+    
+    let upgrade = req.headers().get("Upgrade")?.unwrap_or("".to_string());
+    if upgrade == "websocket".to_string() {
+        let WebSocketPair { server, client } = WebSocketPair::new()?;
+        server.accept()?;
+    
+        wasm_bindgen_futures::spawn_local(async move {
+            let events = server.events().unwrap();
+            if let Err(e) = ProxyStream::new(cx.data, &server, events).process().await {
+                console_log!("[tunnel]: {}", e);
+            }
+        });
+    
+        Response::from_websocket(client)
+    } else {
+        Response::from_html("hi from wasm!")
+    }
+}
